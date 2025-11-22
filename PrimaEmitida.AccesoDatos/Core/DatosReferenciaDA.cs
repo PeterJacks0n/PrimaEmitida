1296033327878dfa5cc3de7831bff8d1e010c628
PeterJacks0n <pedrofsanchezuribe@gmail.com>
2025-11-22 10:17:53 -0500

    feat(data-access): implementar capa de acceso a datos
    - Configurar connection strings en Web.config de WebServices y ClienteWeb
    - Implementar DatosReferenciaDA para operaciones de referencia
    - Implementar DatosTransaccionalesDA para operaciones transaccionales
    - Establecer estructura base para acceso a datos en capa Core
    
    FASE 5 COMPLETADA: Capa de acceso a datos implementada

---
+++ PrimaEmitida.AccesoDatos/Core/DatosReferenciaDA.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrimaEmitida.Entidades.Core;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PrimaEmitida.AccesoDatos.Core
+{
+    public class DatosReferenciaDA
+    {
+        private string ConnectionString =>
+            ConfigurationManager.ConnectionStrings[
+                ConfigurationManager.AppSettings["cnnSql"]
+            ].ConnectionString;
+
+        public List<DatosReferencia> ObtenerTodos()
+        {
+            List<DatosReferencia> lista = new List<DatosReferencia>();
+
+            using (SqlConnection conexion = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand comando = new SqlCommand("sp_ObtenerDatosReferencia", conexion))
+                {
+                    comando.CommandType = CommandType.StoredProcedure;
+                    conexion.Open();
+
+                    SqlDataReader reader = comando.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        lista.Add(new DatosReferencia
+                        {
+                            IdReferencia = Convert.ToInt32(reader["IdReferencia"]),
+                            IdPeriodo = reader["IdPeriodo"].ToString(),
+                            Presentacion = reader["Presentacion"].ToString(),
+                            PrimaEmitida = reader["PrimaEmitida"] != DBNull.Value
+                                ? Convert.ToDecimal(reader["PrimaEmitida"])
+                                : (decimal?)null,
+                            PrimaContabilidad = Convert.ToDecimal(reader["PrimaContabilidad"]),
+                            Diferencia = reader["Diferencia"] != DBNull.Value
+                                ? Convert.ToDecimal(reader["Diferencia"])
+                                : (decimal?)null
+                        });
+                    }
+                    conexion.Close();
+                }
+            }
+            return lista;
+        }
+
+        public List<DatosReferencia> ActualizarYComparar()
+        {
+            List<DatosReferencia> lista = new List<DatosReferencia>();
+
+            using (SqlConnection conexion = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand comando = new SqlCommand("sp_ActualizarYCompararPrimas", conexion))
+                {
+                    comando.CommandType = CommandType.StoredProcedure;
+                    conexion.Open();
+
+                    SqlDataReader reader = comando.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        lista.Add(new DatosReferencia
+                        {
+                            IdReferencia = Convert.ToInt32(reader["IdReferencia"]),
+                            IdPeriodo = reader["IdPeriodo"].ToString(),
+                            Presentacion = reader["Presentacion"].ToString(),
+                            PrimaEmitida = reader["PrimaEmitida"] != DBNull.Value
+                                ? Convert.ToDecimal(reader["PrimaEmitida"])
+                                : (decimal?)null,
+                            PrimaContabilidad = Convert.ToDecimal(reader["PrimaContabilidad"]),
+                            Diferencia = reader["Diferencia"] != DBNull.Value
+                                ? Convert.ToDecimal(reader["Diferencia"])
+                                : (decimal?)null
+                        });
+                    }
+                    conexion.Close();
+                }
+            }
+            return lista;
+        }
+    }
+}
---
+++ PrimaEmitida.AccesoDatos/Core/DatosTransaccionalesDA.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrimaEmitida.Entidades.Core;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PrimaEmitida.AccesoDatos.Core
+{
+    public class DatosTransaccionalesDA
+    {
+        private string ConnectionString =>
+            ConfigurationManager.ConnectionStrings[
+                ConfigurationManager.AppSettings["cnnSql"]
+            ].ConnectionString;
+
+        public bool InsertarMasivo(List<DatosTransaccionales> datos)
+        {
+            using (SqlConnection conexion = new SqlConnection(ConnectionString))
+            {
+                conexion.Open();
+                using (SqlTransaction transaccion = conexion.BeginTransaction())
+                {
+                    try
+                    {
+                        foreach (var item in datos)
+                        {
+                            using (SqlCommand comando = new SqlCommand(
+                                "INSERT INTO DatosTransaccionales (PolTxtPresentacion, PolNumPrima) VALUES (@Presentacion, @Prima)",
+                                conexion, transaccion))
+                            {
+                                comando.Parameters.AddWithValue("@Presentacion",
+                                    item.PolTxtPresentacion ?? (object)DBNull.Value);
+                                comando.Parameters.AddWithValue("@Prima", item.PolNumPrima);
+                                comando.ExecuteNonQuery();
+                            }
+                        }
+                        transaccion.Commit();
+                        return true;
+                    }
+                    catch
+                    {
+                        transaccion.Rollback();
+                        throw;
+                    }
+                }
+            }
+        }
+
+        public bool LimpiarDatos()
+        {
+            using (SqlConnection conexion = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand comando = new SqlCommand("DELETE FROM DatosTransaccionales", conexion))
+                {
+                    conexion.Open();
+                    comando.ExecuteNonQuery();
+                    conexion.Close();
+                    return true;
+                }
+            }
+        }
+    }
+}
